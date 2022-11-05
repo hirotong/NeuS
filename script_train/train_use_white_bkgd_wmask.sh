b8c257a76cdf907a4b08d06a8f2e683a4c5424ec
hiro <jinguang.tong@anu.edu.au>
2022-11-06 01:49:25 +1100

    sync

--- script_train/train_use_white_bkgd_wmask.sh
+++ script_train/train_use_white_bkgd_wmask.sh
@@ -2,7 +2,7 @@
 
 for i in `cat list.txt`
 do
-echo $i
-CUDA_VISIBLE_DEVICES=1 python exp_runner.py --conf confs/insect_use_white_bkgd_wmask.conf --case $i/wobox &&
-python exp_runner.py --conf confs/insect_white_bkgd.conf --case $i/wobox --mode validate_mesh
+echo "working on $i"
+CUDA_VISIBLE_DEVICES=$1 python exp_runner.py --conf confs/insect_use_white_bkgd_wmask.conf --case $i/withbox &&
+python exp_runner.py --conf confs/insect_white_bkgd.conf --case $i/withbox --mode validate_mesh -r 512
 done
